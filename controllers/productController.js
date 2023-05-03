e28846808f1f1746ef517787c30af80eb181fd89
Shawon Talukder <shawontalukder.study@gmail.com>
2023-05-04 01:10:58 +0600

    new model created, products

--- controllers/productController.js
+++ controllers/productController.js
@@ -10,35 +10,47 @@
  */
 
 // Dependencies
+const Product = require("../models/Product");
 
 // Model Scaffolding
 const productController = {};
 
 // Model Structure
 // get all products
-productController.getProducts = (req, res) => {
+productController.getProducts = async (req, res) => {
+  try {
+    const result = await Product.find();
+    if (result) {
+      return res.status(200).json({ data: result });
+    } else {
+      return res.status(500).json({ message: "Server side error!!" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server side error!" });
+  }
   res.status(200).json({ message: "I will send All Products" });
 };
 
-// get single product by its id
-productController.getProduct = (req, res) => {
+// get single product by it's id
+productController.getProduct = async (req, res) => {
   res
     .status(200)
     .json({ message: `I will send single Product for ${req.params.id}` });
 };
 
 // create a product
-productController.createProduct = (req, res) => {
+productController.createProduct = async (req, res) => {
   res.status(200).json({ message: `I will create a Product` });
 };
 
 // update a product
-productController.updateProduct = (req, res) => {
+productController.updateProduct = async (req, res) => {
   res.status(200).json({ message: `I will update a Product ${req.params.id}` });
 };
 
-// update a product
-productController.deleteProduct = (req, res) => {
+// delete a product
+productController.deleteProduct = async (req, res) => {
   res.status(200).json({ message: `I will delete a Product ${req.params.id}` });
 };
 
---
+++ models/Product.js
@@ -0,0 +1,66 @@
+/*
+ *
+ *
+ ------->Title: Product Model
+ ->Description: 
+ ------>Author: Shawon Talukder
+ -------->Date: 05/04/2023
+ *
+ *
+ */
+
+// Dependencies
+const mongoose = require("mongoose");
+
+//schema
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    inStock: {
+      type: Boolean,
+      default: true,
+    },
+    images: {
+      type: Array,
+      required: true,
+    },
+    reviews: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "Review",
+      },
+    ],
+    qna: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "QnA",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+// Model Structure
+const Product = new mongoose.model("Product", productSchema);
+
+// Export Model
+module.exports = Product;
