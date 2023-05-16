5af246eb050496f0ef3972428ac5c72c55fffc76
Shawon Talukder <shawontalukder.study@gmail.com>
2023-05-17 00:54:40 +0600

    model/review created

---
+++ models/Review.js
@@ -0,0 +1,39 @@
+/*
+ *
+ *
+ ------->Title: Review Model
+ ->Description: 
+ ------>Author: Shawon Talukder
+ -------->Date: 05/17/2023
+ *
+ *
+ */
+
+// Dependencies
+const mongoose = require("mongoose");
+
+//schema
+const reviewSchema = new mongoose.Schema(
+  {
+    rating: {
+      type: Number,
+      required: true,
+    },
+    comment: {
+      type: string,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+// Model Structure
+const Review = new mongoose.model("Review", reviewSchema);
+
+// Export Model
+module.exports = Review;
